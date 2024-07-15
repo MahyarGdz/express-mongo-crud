55da8cb49e563463b5fc255daa207432b496d583
mahyargdz <mahyar.goudarzi@outlook.com>
2024-07-16 02:02:17 +0330

    fix(bug): fix guard middleware bug

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,6 +3,6 @@ export default {
   rules: {
     "subject-case": [2, "always", ["sentence-case", "start-case", "pascal-case", "upper-case", "lower-case", "camel-case"]],
     "type-enum": [2, "always", ["add", "build", "chore", "update", "docs", "feat", "fix", "refactor", "revert", "style", "test", "sample"]],
-    "scope-enum": [1, "always", ["common", "core", "app", "testing"]],
+    "scope-enum": [1, "always", ["common", "core", "app", "testing", "bug"]],
   },
 };
